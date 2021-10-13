624e0a50305715aba4da4e1219fd56030b1eca99
BigK_ACE <91551238+bigkace@users.noreply.github.com>
2021-10-13 15:02:09 +0700

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+const int MAX = 100;
+
+void ArrayInput(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        printf("\narr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+}
+
+void Swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void BubbleSort(int arr[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                Swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+void PrintArray(int arr[], int n){
+    printf("\nBubble Sort : ");
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+int main(){
+    int arr[MAX];
+    int n;
+    
+    printf("Enter the number of elements in the array :");
+    do{
+        scanf("%d", &n);
+        if (n <= 0 || n > MAX){
+            printf("Re-enter the number of the elements in the array: ");
+        }
+    }
+    while(n <= 0 || n > MAX);
+
+    printf("\n========ArrayInput=========\n");
+    ArrayInput(arr, n);
+
+    BubbleSort(arr, n);
+    PrintArray(arr, n); 
+}
